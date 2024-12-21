d2ec85cf8357bb6dea7f3bd198f6947bacc79853
Vijay <vijaymudaliyar224@gmail.com>
2024-12-21 14:32:45 -0800

    gsap animation

--- public/js/script.js
+++ public/js/script.js
@@ -3,15 +3,50 @@ gsap.from('.secondhalf',{
     duration:2
 });
 
-gsap.from('.fluid',{
-    x:-600,
+gsap.from('.fluid1',{
+    y:-600,
+    rotateX:'180deg',
+    rotateY:'300deg',
     duration:0.9,
+    opacity:0,
     scrollTrigger:{
-        trigger:'.fluid',
+        trigger:'.fluid1',
         scroller:'body',
         markers:true,
-        start:'top 90%',
-        end:'top 50%',
+        start:'bottom 10%',
+        end:'top 30%',
+        scrub:3
+    }
+})
+gsap.from('.fluid2',{
+    y:-400,
+    rotateX:'-300deg',
+    rotateY:'190deg',
+    opacity:0,
+    borderRadius:'50%',
+    duration:2,
+    scrollTrigger:{
+        trigger:'.fluid2',
+        scroller:'body',
+        markers:true,
+        start:'top 10%',
+        end:'top 30%',
+        scrub:5
+    }
+})
+gsap.from('.fluid3',{
+    x:-200,
+    // y:-100,
+    rotateX:'-180deg',
+    rotateY:'300deg',
+    duration:2,
+    opacity:0.8,
+    scrollTrigger:{
+        trigger:'.fluid3',
+        scroller:'body',
+        markers:true,
+        start:'top 80%',
+        end:'top 30%',
         scrub:5
     }
 })
